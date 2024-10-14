982c474d304a8223c3befb16a24c79317773d2e7
karandesai1404 <34541170-karandesai1404@users.noreply.replit.com>
2024-10-14 13:34:32 +0000

    Search Engine

--- query.js
+++ query.js
@@ -1,5 +1,10 @@
-function submit() {
-	
+function submit() {document.getElementById('buttons').style.display = 'block';
+document.getElementById('content').innerHTML = '';
+var val = document.getElementById('search').value;
+var newelement = document.createElement('script');
+newelement.src = `https://www.googleapis.com/customsearch/v1?key=AIzaSyA_fr7xBpojC0VayzolxPH90nli4b1iGSs&cx=003606982592251140240:5xbiwoxb3m0&q=${val}&callback=hndlr`;
+newelement.id = 'mainscript';
+document.body.appendChild(newelement);	
 }
 
 
